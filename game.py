ba5f323d2217e966cc1e444835d4a4caff42ea01
Kenny Tang <kenny.t4ng@gmail.com>
2024-11-21 23:37:18 +0700

    Add codes for buttons and game over

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from balloons import Balloons
 from map import Map
 from monkeyMenu import MonkeyMenu
 
+
 WIDTH,HEIGHT = 1280,720
 pygame.init()
 pygame.display.set_caption("Tower Defense Window")
@@ -13,14 +14,18 @@ font = pygame.font.Font("assets/font/Pixeltype.ttf",50)
 
 #variables
 put_monkey = False
-buy_monkey = 0
-upgrade_monkey = 1
+buy_monkey = 150
+upgrade_monkey = 100
+sell_monkey = 120
 spawn_cooldown = 400
 message_duration = 2000
+game_outcome = 0
+total_wave = 20
 last_balloon_spawn = pygame.time.get_ticks()
 selected_monkey = None
 invalid_time = None
 wave_started = False
+game_over = False
 running = True
 
 #Map
@@ -54,15 +59,25 @@ monkey_group = pygame.sprite.Group()
 monkeymenu_button = pygame.image.load("assets/Images/GameMenu/buyMonkey.png").convert()
 cancelmenu_button = pygame.image.load("assets/Images/GameMenu/cancelMenu.png").convert_alpha()
 upgrade_monkey_button = pygame.image.load("assets/Images/GameMenu/upgradeMonkey.png").convert_alpha()
-start_button = pygame.image.load("assets/Images/GameMenu/upgradeMonkey.png").convert_alpha()
+sell_button = pygame.image.load("assets/Images/GameMenu/sell.png").convert_alpha()
+start_button = pygame.image.load("assets/Images/GameMenu/beginButton.png").convert_alpha()
+fast_forward_button = pygame.image.load("assets/Images/GameMenu/FastForward.png").convert_alpha()
+restart_button = pygame.image.load("assets/Images/GameMenu/restartButton.png").convert_alpha()
+
+
+health_image = pygame.image.load("assets/Images/GameMenu/Health.png").convert_alpha()
+coin_image = pygame.image.load("assets/Images/GameMenu/Coin.png").convert_alpha()
 
 monkeymenu = MonkeyMenu(monkeymenu_button,1000,10,True)
 cancelmenu = MonkeyMenu(cancelmenu_button,1000,150,True)
-upgrademonkey = MonkeyMenu(upgrade_monkey_button,1000,250,True)
-startbutton = MonkeyMenu(start_button,1150,650,True) 
+upgrademonkey = MonkeyMenu(upgrade_monkey_button,1150,60,True)
+sellbutton = MonkeyMenu(sell_button,1150,110,True)
+startbutton = MonkeyMenu(start_button,1150,620,True) 
+fastforwardbutton = MonkeyMenu(fast_forward_button,1150,620,False) 
+restartbutton = MonkeyMenu(restart_button,500,330,True)
 
 def draw_text(text, text_color, outline_color, x, y):
-    outline_width = 2  
+    outline_width = 2 
 
     for i in range(-outline_width, outline_width+1):
         for j in range(-outline_width, outline_width+1):
@@ -85,54 +100,100 @@ def clear_select():
         monkey.selected = False
 
 while running:
-    balloons_group.update(map)
-    monkey_group.update(balloons_group)
+    if game_over == False:
+        if map.health <= 0:
+            game_over = True
+            game_outcome = -1
+        if map.level + 1 > total_wave:
+            game_over = True
+            game_outcome = 1
 
-    map.draw(screen)
-    balloons_group.draw(screen)
+        balloons_group.update(map)
+        monkey_group.update(balloons_group,map)
 
-    if wave_started != True:
-        if startbutton.draw(screen):
-            wave_started = True
+        map.draw(screen)
+        balloons_group.draw(screen)
+
+    if game_over == False:
+        if wave_started != True:
+            if startbutton.draw(screen):
+                wave_started = True
+        else:
+            map.game_speed = 1
+
+            if fastforwardbutton.draw(screen):
+                map.game_speed = 2
+
+
+            if pygame.time.get_ticks() - last_balloon_spawn > spawn_cooldown:
+                if map.spawned_balloons < len(map.balloon_list):
+                    balloon_type = map.balloon_list[map.spawned_balloons]
+                    balloons = Balloons(balloon_type,balloons_images,path)
+                    balloons_group.add(balloons)
+                    map.spawned_balloons += 1
+                    last_balloon_spawn = pygame.time.get_ticks()
+
+        if map.check_wave_complete() :
+            map.money += 100
+            map.level += 1
+            wave_started = False
+            last_balloon_spawn = pygame.time.get_ticks()
+            map.reset_wave()
+            map.process_balloons()
+
+        for monkey in monkey_group:
+            monkey.draw(screen)
+
+        if monkeymenu.draw(screen):
+            put_monkey = True
+    
+        if put_monkey:
+            if cancelmenu.draw(screen):
+                put_monkey = False
+                print("Cancel")
+            cursor_rect = monkey_image.get_rect()
+            cursor_position = pygame.mouse.get_pos()
+            cursor_rect.midbottom = cursor_position
+            if cursor_position[0] <= 975:
+                screen.blit(monkey_image,cursor_rect)
+
+        if selected_monkey:
+            if selected_monkey.upgrade_level < 4:
+                screen.blit(coin_image,(1140,10))
+                draw_text("$100","white","black",1190,20)
+                if upgrademonkey.draw(screen):
+                    if map.money >= upgrade_monkey:
+                        selected_monkey.upgrade()
+                        map.money -= upgrade_monkey
+            if sellbutton.draw(screen):
+                selected_monkey.sell_monkey()
+                selected_monkey = None
+                map.money += sell_monkey
     else:
-        if pygame.time.get_ticks() - last_balloon_spawn > spawn_cooldown:
-            if map.spawned_balloons < len(map.balloon_list):
-                balloon_type = map.balloon_list[map.spawned_balloons]
-                balloons = Balloons(balloon_type,balloons_images,path)
-                balloons_group.add(balloons)
-                map.spawned_balloons += 1
-                last_balloon_spawn = pygame.time.get_ticks()
+        pygame.draw.rect(screen,"dodgerblue",(300,200,500,300),border_radius = 30)
+        if game_outcome == -1:
+            draw_text("GAME OVER","red","black",470,230)
+            draw_text("Restart","white","black",490,440)
 
-    if map.check_wave_complete() :
-        map.level += 1
-        wave_started = False
-        last_balloon_spawn = pygame.time.get_ticks()
-        map.reset_wave()
-        map.process_balloons()
+        elif game_outcome == 1:
+            draw_text(" YOU WIN !","yellow","black",480,230)
+            draw_text("Restart","white","black",490,440)
 
-    for monkey in monkey_group:
-        monkey.draw(screen)
-
-    if monkeymenu.draw(screen):
-        put_monkey = True
-   
-    if put_monkey:
-        if cancelmenu.draw(screen):
+        if restartbutton.draw(screen):
+            game_over = False
+            wave_started = False
             put_monkey = False
-            print("Cancel")
-        cursor_rect = monkey_image.get_rect()
-        cursor_position = pygame.mouse.get_pos()
-        cursor_rect.midbottom = cursor_position
-        if cursor_position[0] <= 975:
-            screen.blit(monkey_image,cursor_rect)
+            selected_monkey = None
+            last_balloon_spawn = pygame.time.get_ticks()
+            map = Map(map_image)
+            map.process_balloons()
 
-    if selected_monkey:
-        if selected_monkey.upgrade_level < 4:
-            if upgrademonkey.draw(screen):
-                if map.money >= upgrade_monkey:
-                    selected_monkey.upgrade()
-                    map.money -= upgrade_monkey
+            balloons_group.empty()
+            monkey_group.empty()
 
+    # draw_text(str(map.health),"white","black",70,20)
+    # draw_text("$"+ str(map.money),"white","black",200,20)
+    # draw_text("Wave "+ str(map.level),"yellow","black",500,20)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -165,9 +226,17 @@ while running:
         else:
             invalid_time = None  
     
+    def name():
+        if map.level <= 19:
+            return map.level + 1
+        else:
+            return 20
+
+    screen.blit(health_image,(30,15))
+    screen.blit(coin_image,(150,10))
     draw_text(str(map.health),"white","black",70,20)
     draw_text("$"+ str(map.money),"white","black",200,20)
-    draw_text("Wave "+ str(map.level),"yellow","black",500,20)
+    draw_text("Wave "+ str(name()),"yellow","black",500,20)
     # pygame.draw.lines(screen,"grey0", False, path)
     pygame.display.update()
     clock.tick(60)
