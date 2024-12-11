f5b0cb1b9536670da99c01e8c6653a8f9f0e0b16
Kenny Tang <kenny.t4ng@gmail.com>
2024-12-11 23:13:05 +0700

    Add more wave, add comments, and change health

--- balloonsData.py
+++ balloonsData.py
@@ -1,14 +1,15 @@
+# Data for each type of balloon
 balloons_data = {
     "weak":{
-        "health":10,
+        "health":5,
         "speed":2
     },
     "medium":{
-        "health":15,
+        "health":10,
         "speed":3
     },
     "strong":{
-        "health":10,
+        "health":20,
         "speed":4
     },
     "very strong":{
@@ -17,77 +18,126 @@ balloons_data = {
     },
 }
 
+# Number of Balloons that spawn each wave 
 balloons_spawn_data = [
-    {#1
+    {# Wave 1
         "weak":20,
         "medium":0,
         "strong":0,
         "very strong":0
     },
-    {#2
+    {# Wave 2
         "weak":35,
         "medium":0,
         "strong":0,
         "very strong":0
     },
-    {#3
+    {# Wave 3
         "weak":25,
         "medium":5,
         "strong":0,
         "very strong":0
     },
-    {#4
+    {# Wave 4
         "weak":35,
         "medium":15,
         "strong":0,
         "very strong":0
     },
-    {#5
+    {# Wave 5
         "weak":5,
         "medium":25,
         "strong":0,
         "very strong":0
     },
-    {#6
+    {# Wave 6
         "weak":15,
         "medium":15,
         "strong":5,
         "very strong":0
     },
-    {#7
+    {# Wave 7
         "weak":20,
         "medium":20,
         "strong":10,
         "very strong":0
     },
-    {#8
+    {# Wave 8
         "weak":10,
         "medium":20,
         "strong":15,
         "very strong":0
     },
-    {#9
+    {# Wave 9
         "weak":0,
         "medium":0,
         "strong":30,
         "very strong":0
     },
-    {#10
+    {# Wave 10
         "weak":0,
         "medium":100,
         "strong":0,
         "very strong":0
     },
-    {#11
+    {# Wave 11
         "weak":10,
         "medium":10,
         "strong":15,
         "very strong":5
     },
-    {#12
+    {# Wave 12
         "weak":0,
         "medium":15,
         "strong":10,
         "very strong":10
+    },
+    {# Wave 13
+        "weak":0,
+        "medium":50,
+        "strong":25,
+        "very strong":0
+    },
+    {# Wave 14
+        "weak":50,
+        "medium":15,
+        "strong":10,
+        "very strong":10
+    },
+    {# Wave 15
+        "weak":20,
+        "medium":15,
+        "strong":15,
+        "very strong":10
+    },
+    {# Wave 16
+        "weak":0,
+        "medium":0,
+        "strong":40,
+        "very strong":10
+    },
+    {# Wave 17
+        "weak":0,
+        "medium":0,
+        "strong":10,
+        "very strong":15
+    },
+    {# Wave 18
+        "weak":0,
+        "medium":0,
+        "strong":80,
+        "very strong":0
+    },
+    {# Wave 19
+        "weak":0,
+        "medium":0,
+        "strong":10,
+        "very strong":30
+    },
+    {# Wave 20
+        "weak":10,
+        "medium":10,
+        "strong":10,
+        "very strong":50
     },
 ]
