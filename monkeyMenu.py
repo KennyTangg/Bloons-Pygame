2804e9fb9babb7cc11e9382693565f7b437388ea
Kenny Tang <kenny.t4ng@gmail.com>
2024-12-12 10:54:35 +0700

    Add comments

--- monkeyMenu.py
+++ monkeyMenu.py
@@ -1,7 +1,9 @@
 import pygame 
 
+# Defines the MonkeyMenu class
 class MonkeyMenu():
     def __init__(self,image,x,y,single_click):
+        # Set the attributes
         self.image = image
         self.rect = self.image.get_rect(topleft = (x,y))
         self.click = False
@@ -10,8 +12,11 @@ class MonkeyMenu():
     def draw(self,screen):
         position = pygame.mouse.get_pos()
         active = False
+        # if mouse position is in the rectangle the condition will be True
         if self.rect.collidepoint(position):
+            # if left click is press it will return True
             if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                # check if it is toggle or hold 
                 if self.single_click == True:
                     self.click = True
                 active = True
